3451788547c730a5c4cc98a8c7d60f0d764dc748
zooyanki <65367400+zooyanki@users.noreply.github.com>
2021-01-26 22:19:28 +0300

    10

--- backend/app.js
+++ backend/app.js
@@ -23,8 +23,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://zooyanki.students.nomoredomains.rocks');
-  res.header('Access-Control-Allow-Methods','POST, GET, OPTIONS, DELETE, PUT');
-  res.header('Access-Control-Allow-Headers', 'append,delete,entries,foreach,get,has,keys,set,values,Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 
