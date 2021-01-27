88b7804f8f51576b44e8bc0e023dd69a6d32ddaf
zooyanki <65367400+zooyanki@users.noreply.github.com>
2021-01-27 23:41:34 +0300

    34

--- backend/app.js
+++ backend/app.js
@@ -48,8 +48,11 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   }
-
-  next();
+  if (req.method === "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 app.use(express.json({ type: '*/*' }));
