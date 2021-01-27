6c970b5dc9326f349ef042cb167e0bd74a461e50
zooyanki <65367400+zooyanki@users.noreply.github.com>
2021-01-27 23:55:29 +0300

    35

--- backend/app.js
+++ backend/app.js
@@ -47,6 +47,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', 'true');
   }
   if (req.method === "OPTIONS") {
     res.send(200);
