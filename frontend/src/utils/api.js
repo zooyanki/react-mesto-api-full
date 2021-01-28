6d9e60fa550f1eb14ea1b746f10973d8554ae8c4
zooyanki <65367400+zooyanki@users.noreply.github.com>
2021-01-28 21:55:00 +0300

    48

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -82,7 +82,7 @@ export default class Api {
 
 export const api = new Api({ 
   baseUrl: 'http://api.zooyanki.students.nomoredomains.rocks',
-  Authorization : `Bearer ${localStorage.getItem('token', data.token)}`
+  Authorization : `Bearer ${localStorage.getItem('token', data.token)}`,
   credentials: 'include', 
   headers: {    
     'Content-Type': 'application/json' 
