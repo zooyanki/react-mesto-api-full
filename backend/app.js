f500c27aac50fcf611929e40533b67b64ad5371c
zooyanki <65367400+zooyanki@users.noreply.github.com>
2021-01-30 18:32:31 +0300

    final

--- backend/app.js
+++ backend/app.js
@@ -52,6 +52,12 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
